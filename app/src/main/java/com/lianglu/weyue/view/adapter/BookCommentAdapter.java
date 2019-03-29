e0f7d014b09ce550a551ad82a4b5e63bf96ca99a
LeLe673893702 <673893702@qq.com>
2019-03-29 21:57:36 +0800

    添加到图书评论

--- app/src/main/java/com/lianglu/weyue/view/adapter/BookCommentAdapter.java
+++ app/src/main/java/com/lianglu/weyue/view/adapter/BookCommentAdapter.java
@@ -1,9 +1,28 @@
 package com.lianglu.weyue.view.adapter;
 
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.lianglu.weyue.R;
+import com.lianglu.weyue.model.BookCommentBean;
+
+import java.util.List;
+
 /**
  * @author 17173
  * @what
  * @date 2019/3/29
  */
-public class BookCommentAdapter {
+public class BookCommentAdapter extends BaseQuickAdapter<BookCommentBean, BaseViewHolder> {
+
+
+    public BookCommentAdapter(@Nullable List<BookCommentBean> data) {
+        super( R.layout.item_book_shelf, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, BookCommentBean item) {
+
+    }
 }
